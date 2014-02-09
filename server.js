0ea079d81eb44aa4bc2ac8e756339c3532db6c06
Mike Calvanese <mfcalvanese@gmail.com>
2014-02-09 17:38:14 -0500

    added config and server endpoint

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('HEY!');
+});
+
+var port = process.env.PORT;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
