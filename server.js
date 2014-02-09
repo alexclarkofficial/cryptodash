c7d74bf09bafd1b6c852c2fa669ddde6a98424eb
John Kelleher <johnpkelleher@gmail.com>
2014-02-09 17:55:25 -0500

    price of DOGE

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
-var http = require('http');
+// var http = require('http');
+var request = require('superagent');
 
 var app = express();
 
 app.get('/', function(req, res) {
 
     // http call here
+    request.get('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132')
+      .end(function(r){
+        var x = 0;
+        res.send(r.body.return.markets.DOGE.recenttrades[0].price);
+        // ['return']['markets']['DOGE']['recenttrades'][0]['price']);
+      });
 
-    res.send('HEY!');
+    // res.send('HEY!');
 });
 
 var port = process.env.PORT;
